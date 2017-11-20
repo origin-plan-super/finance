5e69a8383fb1b82ff2d700953c696e84d5990b77
艾尔尼亚-A <1173197065@qq.com>
2017-11-20 23:24:10 +0800

    支付宝路由bug

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -39,16 +39,16 @@ return array(
 'seller_email'=>'support@finance365.com',
 
 //这里是异步通知页面url，提交到项目的Pay控制器的notifyurl方法；
-'notify_url'=>'http://120.78.162.200:12138/finance/home/Pay/notifyurl',
+'notify_url'=>'http://120.78.162.200:12138/finance/index.php/home/Pay/notifyurl',
 
 //这里是页面跳转通知url，提交到项目的Pay控制器的returnurl方法；
-'return_url'=>'http://120.78.162.200:12138/finance/home/Pay/returnurl',
+'return_url'=>'http://120.78.162.200:12138/finance/index.php/home/Pay/returnurl',
 
 //支付成功跳转到的页面，我这里跳转到项目的User控制器，myorder方法，并传参payed（已支付列表）
-'successpage'=>'http://120.78.162.200:12138/finance/home/Zfb/myorder?ordtype=payed',
+'successpage'=>'http://120.78.162.200:12138/finance/index.php/home/Zfb/myorder?ordtype=payed',
 
 //支付失败跳转到的页面，我这里跳转到项目的User控制器，myorder方法，并传参unpay（未支付列表）
-'errorpage'=>'http://120.78.162.200:12138/finance/home/Zfb/myorder?ordtype=unpay',
+'errorpage'=>'http://120.78.162.200:12138/finance/index.php/home/Zfb/myorder?ordtype=unpay',
 ),
 
 );
